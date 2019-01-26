ce6626c01a38ee5eab34f8e8290eef0c30694724
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2019-01-26 16:13:46 +0100

    refactoring code

--- js/script.js
+++ js/script.js
@@ -1,47 +1,37 @@
 console.log("this is script.js");
 const form = document.querySelector("#form-container");
-const streetInput = document.querySelector("#street");
-const cityInput = document.querySelector("#city");
 
 function loadData() {
   event.preventDefault();
-  console.log("you clicked submit");
+  const streetInput = document.querySelector("#street");
+  const cityInput = document.querySelector("#city");
   const body = document.querySelector("body");
   // read street and city names from inputs
   const streetName = streetInput.value;
   const cityName = cityInput.value;
+
   // add the Google street View photo
   const address = getStreetView(streetName, cityName);
-  // add in NYTimes results
-  const nytHeaderElem = document.querySelector("#nytimes-header");
-  const nytElem = document.querySelector("#nytimes-articles");
+
+  // get NYT data
   const nytApiKey = config.nyt_api_key;
   const nytUrl =
     "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
     cityName +
     "&api-key=" +
     nytApiKey;
-
-  xhrRequest("GET", nytUrl, getNYTarticles);
+  xhrRequest("GET", nytUrl, extractNYTData);
 
   // get wiki data
-  const wikiElem = document.querySelector("#wikipedia-links");
   const wikiUrl =
     "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
     cityName +
     "&origin=*";
-  xhrRequest("GET", wikiUrl, getWikiArticles);
+  xhrRequest("GET", wikiUrl, extractWikiData);
 
   // clear out old data before new request
-  streetName.textContent = "";
-  cityName.textContent = "";
-  wikiElem.textContent = "";
-  nytElem.textContent = "";
-
-  body.append(
-    "Google StreeView images used to be accessible by just inserting the api url for the correct location, so imagine the background of this page as an image for: " +
-      address
-  );
+  streetName = "";
+  cityName = "";
 }
 
 form.addEventListener("submit", () => {
@@ -49,19 +39,20 @@ form.addEventListener("submit", () => {
 });
 
 const getStreetView = (street, city) => {
+  const comigSoon = document.querySelector("#coming-soon");
   const greeting = document.querySelector("#greeting");
   const address = street + ", " + city;
   greeting.textContent = "So, you want to live at " + address + "?";
-  return address;
+  comigSoon.append(
+    "Google StreeView images used to be accessible by just inserting the api url for the correct location, so imagine the background of this page as an image for: " +
+      address
+  );
 };
 
 const xhrRequest = (method, url, cb) => {
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
-    // console.log("this is xhr readyState:", xhr.readyState);
-    // console.log("this is xhr status:", xhr.status);
     if (xhr.readyState === 4 && xhr.status === 200) {
-      // console.log("this is xhr response:", xhr.responseText);
       cb(JSON.parse(xhr.responseText));
     } else {
       console.log("no reponse from server");
@@ -72,38 +63,37 @@ const xhrRequest = (method, url, cb) => {
   xhr.send();
 };
 
-const getNYTarticles = newsData => {
-  console.log("hi, you're in getNYTarticles");
-  console.log("this is xhr data", newsData);
+const extractNYTData = newsData => {
   const articles = newsData.response.docs;
-  console.log("this is articles", articles);
-
-  const nytHeaderElem = document.querySelector("#nytimes-header");
   const nytElem = document.querySelector("#nytimes-articles");
-
   articles.forEach(article => {
-    const liEl = document.createElement("li");
-    liEl.textContent = article.headline.main;
-    console.log(article.headline.main);
-    nytElem.appendChild(liEl);
+    const newsHeaders = [];
+    newsHeaders.push(article.headline.main);
+    const newsLinks = [];
+    newsLinks.push(article.web_url);
+    createElements(nytElem, newsHeaders, newsLinks);
   });
 };
 
-const getWikiArticles = wikiData => {
+const extractWikiData = wikiData => {
   const wikiElem = document.querySelector("#wikipedia-links");
-  console.log("this is xhr data", wikiData);
   const wikiHeaders = wikiData[1];
-  console.log("this is wiki headers", wikiHeaders);
-  wikiHeaders.forEach((header, i) => {
-    console.log("this is wiki header in the loop", header);
-    console.log("this is i in the loop", i);
-    const wikiLi = document.createElement("li");
-    wikiLi.textContent = header;
-    wikiElem.appendChild(wikiLi);
-  });
   const wikiLinks = wikiData[3];
-  console.log("this is wiki links", wikiLinks);
-  wikiLinks.forEach((link, i) => {
-    console.log("this is wiki link", link);
+  createElements(wikiElem, wikiHeaders, wikiLinks);
+};
+
+const createElements = (parentElement, headersArray, linksArray) => {
+  headersArray.forEach((item, i) => {
+    const newElement = document.createElement("a");
+    newElement.textContent = item;
+    newElement.setAttribute("href", linksArray[i]);
+    newElement.classList.add("wikipedia-li");
+    const newLi = document.createElement("li");
+    parentElement.appendChild(newLi);
+    newLi.appendChild(newElement);
   });
+};
+
+const clearInput = elementName => {
+  elementName.textContent = "";
 };
